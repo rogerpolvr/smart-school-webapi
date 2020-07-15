79d6d93cffe1ef105a0afa2944a5c4a3891863e7
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-15 19:13:47 -0300

    Developing the HttpGet methods for Student Controller

--- SmartSchool.WebAPI/Controllers/StudentController.cs
+++ SmartSchool.WebAPI/Controllers/StudentController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
@@ -6,13 +9,49 @@ namespace SmartSchool.WebAPI.Controllers
     [Route("/[controller]")]
     public class StudentController : ControllerBase
     {
+        public List<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                Name = "Roger",
+                Surname = "Oliveira",
+                PhoneNumber = "12345678"
+            },
+            new Student()
+            {
+                Id = 2,
+                Name = "Fernando",
+                Surname = "Binkowski",
+                PhoneNumber = "87654321"
+            },
+            new Student()
+            {
+                Id = 3,
+                Name = "Mariely",
+                Surname = "Becker",
+                PhoneNumber = "12348765"
+            },
+        };
 
         public StudentController() { }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Students: Marta, Paulo, Johnsons");
+            return Ok(students);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null) return BadRequest("Student id not found.");
+
+            return Ok(student);
+        }
+
+
     }
 }
