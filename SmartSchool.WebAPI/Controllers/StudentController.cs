3983015c32354388348bf05f2ad2f3b419a807d4
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-16 18:04:17 -0300

    Implementing the Get and Post methods for StudentController

--- SmartSchool.WebAPI/Controllers/StudentController.cs
+++ SmartSchool.WebAPI/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Data;
 using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
@@ -9,43 +10,23 @@ namespace SmartSchool.WebAPI.Controllers
     [Route("/[controller]")]
     public class StudentController : ControllerBase
     {
-        public List<Student> students = new List<Student>()
-        {
-            new Student()
-            {
-                Id = 1,
-                Name = "Roger",
-                Surname = "Oliveira",
-                PhoneNumber = "12345678"
-            },
-            new Student()
-            {
-                Id = 2,
-                Name = "Fernando",
-                Surname = "Binkowski",
-                PhoneNumber = "87654321"
-            },
-            new Student()
-            {
-                Id = 3,
-                Name = "Mariely",
-                Surname = "Becker",
-                PhoneNumber = "12348765"
-            },
-        };
+        private readonly DataContext context;
 
-        public StudentController() { }
+        public StudentController(DataContext context)
+        {
+            this.context = context;
+        }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(students);
+            return Ok(context.Students);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var student = students.FirstOrDefault(s => s.Id == id);
+            var student = context.Students.FirstOrDefault(s => s.Id == id);
 
             if (student == null) return BadRequest("Student id not found.");
 
@@ -55,24 +36,26 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpPost]
         public IActionResult Post(Student student)
         {
-            Student newStudent = new Student();
-            newStudent = student;
+            if (student == null) return BadRequest("Student must be not empty.");
 
-            students.Add(newStudent);
+            context.Students.Add(student);
+
+            context.SaveChangesAsync();
 
             return Ok(student);
+
         }
 
         [HttpPut("{id}")]
         public IActionResult Put(int id, Student student)
         {
-            return Ok(student);
+            return Ok();
         }
 
         [HttpPatch("{id}")]
         public IActionResult Patch(int id, Student student)
         {
-            return Ok(student);
+            return Ok();
         }
 
         [HttpDelete("{id}")]
