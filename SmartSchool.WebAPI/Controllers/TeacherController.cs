67151b19d9b52f452a5a3ecdac4be9725dc860ac
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-18 18:30:54 -0300

    Implementing the TeacherController

--- SmartSchool.WebAPI/Controllers/TeacherController.cs
+++ SmartSchool.WebAPI/Controllers/TeacherController.cs
@@ -1,4 +1,8 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Data;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
@@ -6,12 +10,65 @@ namespace SmartSchool.WebAPI.Controllers
     [Route("/[controller]")]
     public class TeacherController : ControllerBase
     {
-        public TeacherController() { }
+        private readonly DataContext context;
+        public TeacherController(DataContext context)
+        {
+            this.context = context;
+        }
+
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Teachers: Pedro, João, Roger");
+            return Ok(context.Teachers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var teacher = context.Teachers.FirstOrDefault(t => t.Id == id);
+
+            if (teacher == null) return BadRequest("Teacher Id is not found.");
+
+            return Ok(teacher);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Teacher teacher)
+        {
+            context.Add(teacher);
+
+            context.SaveChangesAsync();
+
+            return Ok(teacher);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Teacher newTeacherData)
+        {
+            var teacher = context.Teachers.AsNoTracking().FirstOrDefault(t => t.Id == id);
+
+            if (teacher == null) return BadRequest("Teacher Id is not found.");
+
+            context.Update(newTeacherData);
+
+            context.SaveChangesAsync();
+
+            return Ok(newTeacherData);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var teacher = context.Teachers.FirstOrDefault(t => t.Id == id);
+
+            if (teacher == null) return BadRequest("Teacher Id is not found.");
+
+            context.Remove(teacher);
+
+            context.SaveChangesAsync();
+
+            return Ok($"Theacher {id} was deleted.");
         }
     }
 }
