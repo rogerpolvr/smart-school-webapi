95197dd92a37efdb98c308668c3d2ec1d1962aa1
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-29 19:31:44 -0300

    Criando a interface IRepository

---
+++ SmartSchool.WebAPI/Data/IRepository.cs
@@ -0,0 +1,10 @@
+namespace SmartSchool.WebAPI.Data
+{
+    public interface IRepository
+    {
+        void Add<T>(T Entity) where T : class;
+        void Update<T>(T Entity) where T : class;
+        void Delete<T>(T Entity) where T : class;
+        void SaveChanges();
+    }
+}
