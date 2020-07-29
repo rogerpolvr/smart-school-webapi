214e4223a2a4bcbf78fe14f9fd5757d5bb802df7
Roger Oliveira <rogerpolvr@gmail.com>
2020-07-29 19:33:28 -0300

    Implementando a interface IRepository em Repository

---
+++ SmartSchool.WebAPI/Data/Repository.cs
@@ -0,0 +1,25 @@
+namespace SmartSchool.WebAPI.Data
+{
+    public class Repository : IRepository
+    {
+        public void Add<T>(T Entity) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete<T>(T Entity) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update<T>(T Entity) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
